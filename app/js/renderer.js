763e5b98f718b681bc3be574ed57730738a851ff
Ruithlzz09 <bisht09sachin@gmail.com>
2020-06-28 12:36:56 +0530

    Added notification support for online/offline detection

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -3,8 +3,12 @@
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
+
+const { app } = require("electron")
+
 // process.
 const updateOnlineStatus = () => {
+    let status = navigator.onLine ? 'online' : 'offline'
     if (navigator.onLine) {
         document.body.style.backgroundColor = 'green'
         document.getElementById('h2-checking').style.display = 'none'
@@ -16,11 +20,20 @@ const updateOnlineStatus = () => {
         document.getElementById('h2-online').style.display = 'none'
         document.getElementById('h2-offline').style.display = 'block'
     }
+
+    let note = new Notification('App',{
+        body: status=='online'? 'You can proceed your work': 'Interruption in  Access. Please continue later.'
+    })
+
+    note.onclick = () => {
+        console.log('Notification clicked!')
+        
+    }
 }
 
 window.addEventListener('online', updateOnlineStatus)
 window.addEventListener('offline', updateOnlineStatus)
-document.getElementById('checkStatusButton').addEventListener('click',()=>{
+document.getElementById('checkStatusButton').addEventListener('click', () => {
     console.log('checking internet access status')
     updateOnlineStatus()
 })
